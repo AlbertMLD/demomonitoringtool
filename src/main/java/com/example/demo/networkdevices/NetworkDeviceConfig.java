0946cc730d3013c00434850b508cbd9f32ee7bfd
Albert Lungu <albert.lungu@orange.com>
2024-06-05 22:21:14 +0300

    adding 10 device to network devices

--- src/main/java/com/example/demo/networkdevices/NetworkDeviceConfig.java
+++ src/main/java/com/example/demo/networkdevices/NetworkDeviceConfig.java
@@ -37,7 +37,104 @@ public class NetworkDeviceConfig {
                     LocalDateTime.now()
             );
 
-            repository.saveAll(List.of(device1, device2));
+            // Add more devices here
+            NetworkDevice device3 = new NetworkDevice(
+                    3,
+                    "Router2",
+                    "192.168.1.3",
+                    "Router",
+                    "Branch Office",
+                    "Cisco",
+                    "v1.0",
+                    "Active",
+                    LocalDateTime.now()
+            );
+
+            NetworkDevice device4 = new NetworkDevice(
+                    4,
+                    "Switch2",
+                    "192.168.1.4",
+                    "Switch",
+                    "Branch Office",
+                    "Cisco",
+                    "v1.0",
+                    "Active",
+                    LocalDateTime.now()
+            );
+
+            NetworkDevice device5 = new NetworkDevice(
+                    5,
+                    "Router3",
+                    "192.168.1.5",
+                    "Router",
+                    "Data Center",
+                    "Cisco",
+                    "v1.0",
+                    "Active",
+                    LocalDateTime.now()
+            );
+
+            NetworkDevice device6 = new NetworkDevice(
+                    6,
+                    "Switch3",
+                    "192.168.1.6",
+                    "Switch",
+                    "Data Center",
+                    "Cisco",
+                    "v1.0",
+                    "Active",
+                    LocalDateTime.now()
+            );
+
+            NetworkDevice device7 = new NetworkDevice(
+                    7,
+                    "Router4",
+                    "192.168.1.7",
+                    "Router",
+                    "Remote Office",
+                    "Cisco",
+                    "v1.0",
+                    "Active",
+                    LocalDateTime.now()
+            );
+
+            NetworkDevice device8 = new NetworkDevice(
+                    8,
+                    "Switch4",
+                    "192.168.1.8",
+                    "Switch",
+                    "Remote Office",
+                    "Cisco",
+                    "v1.0",
+                    "Active",
+                    LocalDateTime.now()
+            );
+
+            NetworkDevice device9 = new NetworkDevice(
+                    9,
+                    "Firewall1",
+                    "192.168.1.9",
+                    "Firewall",
+                    "Main Office",
+                    "Cisco",
+                    "v1.0",
+                    "Active",
+                    LocalDateTime.now()
+            );
+
+            NetworkDevice device10 = new NetworkDevice(
+                    10,
+                    "Firewall2",
+                    "192.168.1.10",
+                    "Firewall",
+                    "Data Center",
+                    "Cisco",
+                    "v1.0",
+                    "Active",
+                    LocalDateTime.now()
+            );
+
+            repository.saveAll(List.of(device1, device2, device3, device4, device5, device6, device7, device8, device9, device10));
         };
     }
 }
